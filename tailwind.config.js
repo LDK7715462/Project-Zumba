03dd6acd4c7aa20017c86dfeea1162f7c09f1717
Blenmus <132626093+Blenmus@users.noreply.github.com>
2023-09-11 13:52:47 +1200

    Add files via upload
    Signed-off-by: Blenmus <132626093+Blenmus@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    colors: {
+      java: {
+        50: "#f0fdfb",
+        100: "#cbfcf5",
+        200: "#97f8ec",
+        300: "#5bede0",
+        400: "#29d8ce",
+        500: "#10bcb5",
+        600: "#0a9795",
+        700: "#0c7978",
+        800: "#0f5f60",
+        900: "#124f4f",
+        950: "#032e30",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
