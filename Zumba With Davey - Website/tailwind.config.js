b18e53e104a88ee4c88a3b48906bd7d13b95d27d
Dylan Martin <119099353+DylanMartino@users.noreply.github.com>
2023-09-12 15:40:36 +1200

    Add files via upload
    Signed-off-by: Dylan Martin <119099353+DylanMartino@users.noreply.github.com>

--- Zumba With Davey - Website/tailwind.config.js
+++ Zumba With Davey - Website/tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*/*.{html,js,php}"],
-   theme: {
-     extend: {},
-},
-   plugins: [],
-}
+  content: [
+    "./*.php",
+    "./*.html",
+    "./js/*.js",
+    "./css/*.css",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        java: {
+          50: "#f0fdfb",
+          100: "#cbfcf5",
+          200: "#97f8ec",
+          300: "#5bede0",
+          400: "#29d8ce",
+          500: "#10bcb5",
+          600: "#0a9795",
+          700: "#0c7978",
+          800: "#0f5f60",
+          900: "#124f4f",
+          950: "#032e30",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
